17761963e813c5904dc6f6ca8f3a38c7fc357a84
dh23iterasys <dh23@iterasys.com.br>
2025-08-12 22:16:22 -0300

    SQLAlchimy

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from routers import aluno
 import logging                  # biblioteca de registro
@@ -6,14 +7,24 @@ import logging                  # biblioteca de registro
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = FastAPI() # instancia o objeto do framework de criação de API
-
-@app.on_event("startup") # iniciar o logging
-def startup_event():
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    # inicialização da API  (ao ligar)
     logger.info("API Iniciada")
 
-@app.on_event("shutdown") # finalizar o logging
-def shutdown_event():
-    logger.info("API Finalizada")
+    yield # representa a aplicação enquanto executada
+
+    # finalização da API    (ao desligar)
+    logger.info("API Finalizada")      
+
+app = FastAPI(lifespan=lifespan) # instancia o objeto do framework de criação de API
+
+# @app.on_event("startup") # iniciar o logging
+# def startup_event():
+#     logger.info("API Iniciada")
+
+# @app.on_event("shutdown") # finalizar o logging
+# def shutdown_event():
+#     logger.info("API Finalizada")
 
 app.include_router(aluno.router, prefix="/buscar-aluno", tags=["Aluno"])
