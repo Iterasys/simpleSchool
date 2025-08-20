13f9d1e8c292086c094e95065760563450522548
dh23iterasys <dh23@iterasys.com.br>
2025-08-20 11:43:57 -0300

    Post e Get com BD

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+import database
+import models
 from routers import aluno
 import logging                  # biblioteca de registro
 
@@ -10,11 +12,16 @@ logger = logging.getLogger(__name__)
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     # inicialização da API  (ao ligar)
+    logger.info("Verifica modelos do banco de dados")
+    async with database.engine.begin() as conn:
+        await conn.run_sync(models.Base.metadata.create_all)
     logger.info("API Iniciada")
 
     yield # representa a aplicação enquanto executada
 
     # finalização da API    (ao desligar)
+    logger.info("Encerra a conexão")
+    await database.engine.dispose()
     logger.info("API Finalizada")      
 
 app = FastAPI(lifespan=lifespan) # instancia o objeto do framework de criação de API
@@ -27,4 +34,4 @@ app = FastAPI(lifespan=lifespan) # instancia o objeto do framework de criação 
 # def shutdown_event():
 #     logger.info("API Finalizada")
 
-app.include_router(aluno.router, prefix="/buscar-aluno", tags=["Aluno"])
+app.include_router(aluno.router, prefix="/aluno", tags=["Aluno"])
--- routers/aluno.py
+++ routers/aluno.py
@@ -1,6 +1,13 @@
 # bibliotecas e frameworks
-from fastapi import APIRouter, HTTPException
+from datetime import date, timedelta
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from pydantic import BaseModel, EmailStr
+from database import get_db_session
+from models import Aluno
+from schemas import AlunoResponse, AlunoRequest
+
 import logging
 
 logger = logging.getLogger(__name__)
@@ -20,21 +27,56 @@ alunos_db = {
     }
 }
 
-class AlunoRequest(BaseModel):
-    email: EmailStr
+# class AlunoRequest(BaseModel):
+#     email: EmailStr
 
-@router.post("/")
-def buscar_aluno_post(dados: AlunoRequest):
-    aluno = alunos_db.get(dados.email)
-    logger.info(f"Consulta realizada para: {dados.email}")
-    if not aluno:
-        raise HTTPException(status_code=404, detail="Aluno não encontrado")
-    return aluno
+# @router.post("/")
+# def buscar_aluno_post(dados: AlunoRequest):
+#     aluno = alunos_db.get(dados.email)
+#     logger.info(f"Consulta realizada para: {dados.email}")
+#     if not aluno:
+#         raise HTTPException(status_code=404, detail="Aluno não encontrado")
+#     return aluno
 
-@router.get("/")
-def buscar_aluno_get(email: EmailStr):
-    aluno = alunos_db.get(email)
+# @router.get("/")
+# def buscar_aluno_get(email: EmailStr):
+#     aluno = alunos_db.get(email)
+#     logger.info(f"Consulta realizada para: {email}")
+#     if not aluno:
+#         raise HTTPException(status_code=404, detail="Aluno não encontrado")
+#     return aluno
+
+@router.post("/", response_model=AlunoResponse, 
+             status_code=201, response_description="Aluno Criado")
+async def criar_aluno(aluno: AlunoRequest, db: AsyncSession = Depends(get_db_session)):
+    # lê a data atual e projeta a data final da assinatura daqui há um ano
+    validade_assinatura = date.today() + timedelta(days=365)
+    # prepara a estrutura de dados para cadastrar o novo aluno
+    novo_aluno = Aluno(
+        nome=aluno.nome,
+        email=aluno.email,
+        senha_hash=aluno.senha,
+        validade_assinatura=validade_assinatura
+    )
+    # Tenta criar o novo aluno banco de dados
+    try:
+        db.add(novo_aluno)
+        await db.commit()
+        await db.refresh(novo_aluno)
+
+    # Se der algo errado, vai desfazer a transação inteira (os 3 passos acima)
+    except Exception as e:
+        await db.rollback() 
+        raise HTTPException(status_code=500, detail="Erro ao salvar o aluno")
+    print(novo_aluno) # exibir dados no aluno
+    return novo_aluno
+
+@router.get("/", response_model=AlunoResponse)
+async def buscar_aluno(email: EmailStr, db: AsyncSession = Depends(get_db_session)):
+    # executa a consulta no banco de dados
+    result = await db.execute(select(Aluno).filter(Aluno.email == email))
+    aluno = result.scalars().first()
     logger.info(f"Consulta realizada para: {email}")
-    if not aluno:
+    if aluno is None:
         raise HTTPException(status_code=404, detail="Aluno não encontrado")
     return aluno
